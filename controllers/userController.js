d74d3fa8df3bdcf5bdee0bfb198c4693de866bc2
dinh <1753003@student.hcmus.edu.vn>
2019-12-15 15:52:23 +0700

    add cookie parser

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,16 @@ let models = require('../models');
 let User = models.User;
 let bcrypt = require('bcryptjs');
 
-controller.getUserByEmail = () =>{
-  return User.findOne({
-    where: {username: '111'}
-  });
-};
-
 controller.createUser = (user) =>{
   var salt = bcrypt.genSaltSync(10);
   user.password = bcrypt.hashSync(user.password, salt);
   return User.create(user);
 };
+controller.getUserByEmail = (email) =>{
+  return User.findOne({
+    where: {username: email}
+  });
+};
+
+module.exports = controller;
+
--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var express = require("express");
 var app = express();
-var bodyParser = require('body-parser')
 
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-// app.use(bodyParser.json())
 // Setting for app here
 app.use(express.static(__dirname));
 
@@ -20,7 +14,23 @@ var hbs = expressHbs.create({
 });
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
-
+//body parser
+let bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+// cookie parser
+let cookieParser = require('cookie-parser');
+app.use(cookieParser());
+//session
+let session = require('express-session');
+app.use(session({
+  cookie: {httpOnly:true, maxAge :30*24*60*60*1000},
+  secret:'s3Cret',
+  resave: false,
+  saveUninitialized: false
+}));
 // Create database
 var models = require("./models");
 app.get("/sync", function(req, res) {
