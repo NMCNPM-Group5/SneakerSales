2555dc323b03c2df0ea074b25269ecc37937f190
1753057 <1753057@student.hcmus.edu.vn>
2019-12-15 13:42:25 +0700

    add view and view detail

--- controllers/sneakerController.js
+++ controllers/sneakerController.js
@@ -3,27 +3,38 @@ let models = require("../models");
 let sneaker = models.Sneaker;
 
 controller.getAll = () => {
-  return new Promise((resolve, reject) => {
-    sneaker
-      .findAll({
-        attributes: [
-          "id",
-          "sneakerID",
-          "name",
-          "brandName",
-          "price",
-          "sizeID",
-          "imgPath",
-          "subImgPath",
-          "description",
-          "status",
-          "color",
-          "gender"
-        ]
-      })
-      .then(data => resolve(data))
-      .catch(error => reject(new Error(error)));
-  });
+    return new Promise((resolve, reject) => {
+        sneaker
+            .findAll({
+                attributes: [
+                    "id",
+                    "sneakerID",
+                    "name",
+                    "brandName",
+                    "price",
+                    "sizeID",
+                    "imgPath",
+                    "subImgPath",
+                    "description",
+                    "status",
+                    "color",
+                    "gender"
+                ]
+            })
+            .then(data => resolve(data))
+            .catch(error => reject(new Error(error)));
+    });
+};
+
+controller.getById = function(id) {
+    return new Promise((resolve, reject) => {
+        sneaker
+            .findOne({
+                where: { id: id },
+            })
+            .then(result => resolve(result))
+            .catch(error => reject(new Error(error)));
+    })
 };
 
 module.exports = controller;
