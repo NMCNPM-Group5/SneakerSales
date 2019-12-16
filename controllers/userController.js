813a44d2e1f1e61c25eca58f880fa718d6d09c58
dinh <1753003@student.hcmus.edu.vn>
2019-12-16 16:16:36 +0700

    working sign up function

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,8 +4,9 @@ let User = models.User;
 let bcrypt = require('bcryptjs');
 
 controller.createUser = (user) =>{
-  var salt = bcrypt.genSaltSync(10);
-  user.password = bcrypt.hashSync(user.password, salt);
+  // console.log(user);
+  // var salt = bcrypt.genSaltSync(10);
+  // user.password = bcrypt.hashSync(user.password, salt);
   return User.create(user);
 };
 controller.getUserByEmail = (email) =>{
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,41 +10,45 @@ router.get("/signUp", (req, res) => {
   res.render("detail/signUp");
 });
 router.post('/signUp', (req, res, next)=>{
-  let user = {
-    fullname: req.body.fullname,
-    username: req.body.username,
-    password: req.body.password,
-  };
+  let fullname= req.body.fullname;
+  let email= req.body.username;
+  let password= req.body.password;
   let confirmPassword= req.body.confirmPassword;
   let keepLoggedIn = (req.body.keepLoggedIn!= undefined);
   console.log(req.body);
-  console.log(user.password);
-  console.log(confirmPassword);
+  // console.log(user.password);
+  // console.log(confirmPassword);
   //Kiem tra confirmpassword va password
-  if( user.password != confirmPassword){
-    return res.render('signUp', {
+  if( password != confirmPassword){
+    return res.render('detail/signUp', {
       message: 'Confirm password does not match',
       type:  'alert-danger'
     });
   };
   //Kiem tra user name valid
   userController
-  .getUserByEmail(user.username)
+  .getUserByEmail(email)
   .then(user=>{
       if(user){
-        return res.render('signUp', {
+        return res.render('detail/signUp', {
           message: `Email ${user.username} exists`,
           type:  'alert-danger'
         });
       }
+      user = {
+        fullname,
+        username: email,
+        password
+      };
       return userController
         .createUser(user)
         .then(user =>{
+          console.log(user)
           if(keepLoggedIn){
             req.session.user = user;
             res.render('/')
           }else{
-            return res.render('login', {
+            return res.render('detail/login', {
               message: 'sign up completed, pls login',
               type: 'alert-primary'
             });
