cdbc29848815bb2473f3b41e91cfc4077a5f29f7
1753048 <1753048@student.hcmus.edu.vn>
2019-12-16 11:26:25 +0700

    fix

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 var express = require("express");
 var app = express();
 
-var bodyParser = require('body-parser');
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-    //Setting for app here
-
+// Setting for app here
 app.use(express.static(__dirname));
 
 var expressHbs = require("express-handlebars");
 
 var hbs = expressHbs.create({
-    extname: "hbs",
-    defaultLayout: "layout",
-    layoutsDir: __dirname + "/views/layouts/",
-    partialsDir: __dirname + "/views/partials/"
+  extname: "hbs",
+  defaultLayout: "layout",
+  layoutsDir: __dirname + "/views/layouts/",
+  partialsDir: __dirname + "/views/partials/"
 });
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
-<<<<<<< HEAD
 //body parser
 let bodyParser = require("body-parser");
 // parse application/x-www-form-urlencoded
@@ -42,49 +33,18 @@ app.use(
     saveUninitialized: false
   })
 );
-=======
-
-
-
-//Use Body Parser
-
-
-//Use Cookie-parser
-let cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
-//Use session
-let session = require('express-session');
-app.use(session({
-    cookie: { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 },
-    secret: 'S3cret',
-    resave: false,
-    saveUninitialized: false
-}));
-
-//Use Cart controller
-let Cart = require('./controllers/cartController');
-app.use((req, res, next) => {
-    var cart = new Cart(req.session.cart ? req.session.cart : {});
-    req.session.cart = cart;
-    res.locals.totalQuantity = cart.totalQuantity;
-    next();
-});
-
-
->>>>>>> fd460c44b46cbdb8d85d09f6ac065c3180413d31
 // Create database
 var models = require("./models");
 app.get("/sync", function(req, res) {
-    models.sequelize.sync().then(function() {
-        res.send("database sync completed!");
-    });
+  models.sequelize.sync().then(function() {
+    res.send("database sync completed!");
+  });
 });
 
+//
 
 // hien thi trang chu
 app.use("/", require("./routes/indexRouter"));
-app.use("/cart", require("./routes/shoppingCartRouter"));
 // hien thi trang chi tiet
 app.use("/search", require("./routes/searchRouter"));
 app.use("/product", require("./routes/productRoutes"));
@@ -94,5 +54,5 @@ app.use("/user", require("./routes/UserRouter"));
 // start server
 app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () => {
-    console.log(`server is listening on port ${app.get("port")}`);
+  console.log(`server is listening on port ${app.get("port")}`);
 });
--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 router.get("/", (req, res, next) => {
   let sneakerController = require("../controllers/sneakerController");
-  sneakerController.getAll(req.query).then(data => {
+  sneakerController.getAllFilter(req.query).then(data => {
     res.locals.sneaker = data;
     res.render("detail/searchResult");
   });
