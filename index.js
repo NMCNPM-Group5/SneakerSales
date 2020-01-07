ca33dbb8b540996d0289cb25c9d411c5c6519ad8
1753048 <1753048@student.hcmus.edu.vn>
2020-01-07 20:53:14 +0700

    test

--- index.js
+++ index.js
@@ -1,73 +1,64 @@
 var express = require("express");
 var app = express();
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
-    //Setting for app here
+app.use(bodyParser.json());
+//Setting for app here
 
 app.use(express.static(__dirname));
 
 var expressHbs = require("express-handlebars");
 
 var hbs = expressHbs.create({
-    extname: "hbs",
-    defaultLayout: "layout",
-    layoutsDir: __dirname + "/views/layouts/",
-    partialsDir: __dirname + "/views/partials/"
+  extname: "hbs",
+  defaultLayout: "layout",
+  layoutsDir: __dirname + "/views/layouts/",
+  partialsDir: __dirname + "/views/partials/"
 });
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 
-
 //Use Body Parser
 
-
 //Use Cookie-parser
-let cookieParser = require('cookie-parser');
+let cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 //Use session
-let session = require('express-session');
-app.use(session({
+let session = require("express-session");
+app.use(
+  session({
     cookie: { httpOnly: true, maxAge: null },
-    secret: 'S3cret',
+    secret: "S3cret",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
 //Use Cart controller
-let Cart = require('./controllers/cartController');
+let Cart = require("./controllers/cartController");
 app.use((req, res, next) => {
-    var cart = new Cart(req.session.cart ? req.session.cart : {});
-    req.session.cart = cart;
-    res.locals.totalQuantity = cart.totalQuantity;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+  req.session.cart = cart;
+  res.locals.totalQuantity = cart.totalQuantity;
 
-    res.locals.username = req.session.user ? req.session.user.username : '';
-<<<<<<< Updated upstream
-
-    res.locals.isLoggedIn = req.session.user ? true : false;
-
-
-
-=======
-    res.locals.isLoggedIn = req.session.user ? true : false;
->>>>>>> Stashed changes
-    next();
+  res.locals.username = req.session.user ? req.session.user.username : "";
+  res.locals.isLoggedIn = req.session.user ? true : false;
+  next();
 });
 
 // Create database
 var models = require("./models");
 app.get("/sync", function(req, res) {
-    models.sequelize.sync().then(function() {
-        res.send("database sync completed!");
-    });
+  models.sequelize.sync().then(function() {
+    res.send("database sync completed!");
+  });
 });
-
 
 // hien thi trang chu
 app.use("/", require("./routes/indexRouter"));
@@ -83,5 +74,5 @@ app.use("/user", require("./routes/UserRouter"));
 // start server
 app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () => {
-    console.log(`server is listening on port ${app.get("port")}`);
+  console.log(`server is listening on port ${app.get("port")}`);
 });
