0fad63cf9df674fed6ac3846a1352597f1e70330
1753048 <1753048@student.hcmus.edu.vn>
2020-01-08 18:10:19 +0700

    Test

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,27 +1,11 @@
 let controller = {};
 let models = require("../models");
-let sneaker = models.Sneaker;
 let brand = models.Brand;
 
 controller.getAll = () => {
   return new Promise((resolve, reject) => {
-    sneaker
-      .findAll({
-        attributes: [
-          "id",
-          "sneakerID",
-          "name",
-          "brandName",
-          "price",
-          "sizeID",
-          "imgPath",
-          "subImgPath",
-          "description",
-          "status",
-          "color",
-          "gender"
-        ]
-      })
+    brand
+      .findAll()
       .then(data => resolve(data))
       .catch(error => reject(new Error(error)));
   });
--- routes/brandRouter.js
+++ routes/brandRouter.js
@@ -2,6 +2,15 @@ let express = require("express");
 let router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("detail/brand");
+  let brandController = require("../controllers/brandController");
+  return brandController
+    .getAll()
+    .then(result => {
+      res.locals.brand = result;
+      res.render("detail/brand");
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 module.exports = router;
